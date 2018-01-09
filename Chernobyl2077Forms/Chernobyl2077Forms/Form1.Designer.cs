b9c3f86db6357a7dc7ede5dfd56fbfd636477a9f
Virk0 <virko.veelmaa@tptlive.ee>
2018-01-09 10:24:13 +0200

    desaidab

--- Chernobyl2077Forms/Chernobyl2077Forms/Form1.Designer.cs
+++ Chernobyl2077Forms/Chernobyl2077Forms/Form1.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 536);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choice 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 536);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Choice 2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(262, 536);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 53);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Choice 3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(387, 536);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 53);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Choice 4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(15, 260);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 5;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 601);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonNext;
     }
 }
 
--- Chernobyl2077Forms/Chernobyl2077Forms/Form1.cs
+++ Chernobyl2077Forms/Chernobyl2077Forms/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Chernobyl2077Forms
 {
@@ -15,6 +18,34 @@ namespace Chernobyl2077Forms
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            string line;
+
+            // Read the file and display it line by line.  
+            string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Script");
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(path + @"\" + @"test.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                counter++;
+                label1.Text = (line);
+            }
+
+            file.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
