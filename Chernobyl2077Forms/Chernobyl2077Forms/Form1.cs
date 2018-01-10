85268d513358022edf060352473f6c1d472a015e
Si1mT <33309403+Si1mT@users.noreply.github.com>
2018-01-10 10:20:10 +0200

    print image

--- Chernobyl2077Forms/Chernobyl2077Forms/Form1.cs
+++ Chernobyl2077Forms/Chernobyl2077Forms/Form1.cs
@@ -27,13 +27,24 @@ namespace Chernobyl2077Forms
 
         public Array splitlines2()
         {
-            // Read the file and display it line by line.  
+            //Read the file and display it line by line.  
             string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Script");
             System.IO.StreamReader file = new System.IO.StreamReader(path + @"\" + @"test.txt");
             string lines1 = file.ReadToEnd();
             string[] splitlines = lines1.Split(new Char[] { '#' });
             file.Close();
             return splitlines;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("dab");
+            string path=Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Script");
+            PictureBox image = new PictureBox();
+            image.Image = Image.FromFile(path + @"\" + @"image.jpg");
+            image.Location = new Point(100, 100);
+            image.Size = new Size(500, 500);
+            this.Controls.Add(image);
         }
 
         private void button5_Click(object sender, EventArgs e)
@@ -43,6 +54,12 @@ namespace Chernobyl2077Forms
                 label1.Text += number;
                 label2.Text += number;
             }
+            string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "Script");
+            PictureBox image = new PictureBox();
+            image.Image = Image.FromFile(path + @"\" + @"image.jpg");
+            image.Location = new Point(100, 100);
+            image.Size = new Size(500, 500);
+            this.Controls.Add(image);
         }
 
         private void label1_Click(object sender, EventArgs e)
