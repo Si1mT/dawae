fdf8885b64d24feb1c549569ee3a881953c3d9fe
Virk0 <virko.veelmaa@tptlive.ee>
2018-01-11 09:16:44 +0200

    Lmao

--- Chernobyl2077Forms/Chernobyl2077Forms/Form1.Designer.cs
+++ Chernobyl2077Forms/Chernobyl2077Forms/Form1.Designer.cs
@@ -40,15 +40,22 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Gainsboro;
             this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(12, 308);
+            this.button1.Location = new System.Drawing.Point(13, 308);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 53);
+            this.button1.Size = new System.Drawing.Size(200, 93);
             this.button1.TabIndex = 0;
             this.button1.Text = "Choice 1";
             this.button1.UseVisualStyleBackColor = false;
@@ -58,9 +65,9 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Gainsboro;
-            this.button2.Location = new System.Drawing.Point(12, 445);
+            this.button2.Location = new System.Drawing.Point(12, 554);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 53);
+            this.button2.Size = new System.Drawing.Size(200, 93);
             this.button2.TabIndex = 1;
             this.button2.Text = "Choice 2";
             this.button2.UseVisualStyleBackColor = false;
@@ -70,9 +77,9 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Gainsboro;
-            this.button3.Location = new System.Drawing.Point(387, 308);
+            this.button3.Location = new System.Drawing.Point(554, 308);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(119, 53);
+            this.button3.Size = new System.Drawing.Size(200, 93);
             this.button3.TabIndex = 2;
             this.button3.Text = "Choice 3";
             this.button3.UseVisualStyleBackColor = false;
@@ -82,9 +89,9 @@
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.Gainsboro;
-            this.button4.Location = new System.Drawing.Point(387, 445);
+            this.button4.Location = new System.Drawing.Point(554, 554);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(119, 53);
+            this.button4.Size = new System.Drawing.Size(200, 93);
             this.button4.TabIndex = 3;
             this.button4.Text = "Choice 4";
             this.button4.UseVisualStyleBackColor = false;
@@ -94,9 +101,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(194, 47);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(267, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.Size = new System.Drawing.Size(45, 24);
             this.label1.TabIndex = 4;
             this.label1.Text = "Blya";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -106,17 +114,19 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
             this.label2.Location = new System.Drawing.Point(13, 13);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.Size = new System.Drawing.Size(80, 33);
             this.label2.TabIndex = 5;
             this.label2.Text = "Quiz";
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(219, 241);
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.button5.Location = new System.Drawing.Point(316, 244);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(127, 91);
             this.button5.TabIndex = 6;
             this.button5.Text = "Start";
             this.button5.UseVisualStyleBackColor = true;
@@ -125,44 +135,52 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(216, 102);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label3.Location = new System.Drawing.Point(134, 93);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.Size = new System.Drawing.Size(29, 20);
             this.label3.TabIndex = 7;
+            this.label3.Text = "     ";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(216, 155);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label4.Location = new System.Drawing.Point(134, 220);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.Size = new System.Drawing.Size(25, 20);
             this.label4.TabIndex = 8;
+            this.label4.Text = "    ";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(281, 102);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label5.Location = new System.Drawing.Point(554, 93);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.Size = new System.Drawing.Size(25, 20);
             this.label5.TabIndex = 9;
+            this.label5.Text = "    ";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(281, 155);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label6.Location = new System.Drawing.Point(554, 220);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.Size = new System.Drawing.Size(25, 20);
             this.label6.TabIndex = 10;
+            this.label6.Text = "    ";
             this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label7.Location = new System.Drawing.Point(194, 60);
+            this.label7.Location = new System.Drawing.Point(327, 158);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(121, 25);
             this.label7.TabIndex = 11;
@@ -170,11 +188,90 @@
             this.label7.Visible = false;
             this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(223, 98);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "----->";
+            this.label8.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(223, 227);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(28, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "----->";
+            this.label9.Visible = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(520, 100);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(28, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "<-----";
+            this.label10.Visible = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(520, 227);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(28, 13);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "<-----";
+            this.label11.Visible = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(328, 341);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(104, 72);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "?Retry";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label12.Location = new System.Drawing.Point(327, 204);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(61, 20);
+            this.label12.TabIndex = 17;
+            this.label12.Text = ":Score";
+            this.label12.Visible = false;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(384, 209);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(25, 13);
+            this.label13.TabIndex = 18;
+            this.label13.Text = "      ";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(518, 548);
+            this.ClientSize = new System.Drawing.Size(1082, 659);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -210,6 +307,13 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
     }
 }
 
--- Chernobyl2077Forms/Chernobyl2077Forms/Form1.cs
+++ Chernobyl2077Forms/Chernobyl2077Forms/Form1.cs
@@ -23,11 +23,16 @@ namespace Chernobyl2077Forms
             label1.Visible = false;
             label7.Visible = false;
         }
+        public static int score;
         public static bool BuIsPr;
         public static bool BuIsPr1;
         public static bool BuIsPr2;
         public static bool BuIsPr3;
         public static bool BuIsPr4;
+        private void label13_Click(object sender, EventArgs e)
+        {
+            label13.Text = score.ToString();
+        }
         public Form1()
         {
             InitializeComponent();
@@ -67,6 +72,11 @@ namespace Chernobyl2077Forms
             this.button2.Visible = true;
             this.button3.Visible = true;
             this.button4.Visible = true;
+            this.label12.Visible = true;
+            this.label13.Location = new System.Drawing.Point(704, 35);
+            this.label13.Size = new System.Drawing.Size(25, 13);
+            this.label12.Location = new System.Drawing.Point(646, 30);
+            this.label12.Size = new System.Drawing.Size(61, 20);
             Quiz1();
            
         }
@@ -196,13 +206,41 @@ namespace Chernobyl2077Forms
         {
             DeBuSet();
             label1.Text = "What is the capital city of UK";
-            label3.Text = "Washington";
+            label3.Text = "Washing a ton";
             label4.Text = "Glasgow";
             label5.Text = "Boston";
             label6.Text = "London";
             checkForBuPr();
+            RiAns4();
+            Quiz9();
         }
-            public static void checkForBuPr()
+        public void Quiz9()
+        {
+            DeBuSet();
+            label1.Text = "How many km² is US?";
+            label3.Text = "9 965 000";
+            label4.Text = "9 834 000";
+            label5.Text = "10 000 000";
+            label6.Text = "9 757 000";
+            checkForBuPr();
+            RiAns2();
+            Quiz10();
+
+        }
+        public void Quiz10()
+        {
+            DeBuSet();
+            label1.Text = "How many km² is UK?";
+            label3.Text = "245 500";
+            label4.Text = "342 495";
+            label5.Text = "242 495";
+            label6.Text = "3 cheeseburgers";
+            checkForBuPr();
+            RiAns3();
+            //Quiz11();
+
+        }
+        public static void checkForBuPr()
         {
             while (BuIsPr == false)
             {
@@ -211,6 +249,15 @@ namespace Chernobyl2077Forms
         }
         public void DeBuSet()
         {
+            label13.Text = score.ToString();
+            new System.Threading.ManualResetEvent(false).WaitOne(1000);
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button3.BackColor = System.Drawing.Color.Gainsboro;
+            this.button4.BackColor = System.Drawing.Color.Gainsboro;
+        }
+        public void DeBuSetNoSlow()
+        {
             this.button1.BackColor = System.Drawing.Color.Gainsboro;
             this.button2.BackColor = System.Drawing.Color.Gainsboro;
             this.button3.BackColor = System.Drawing.Color.Gainsboro;
@@ -218,21 +265,47 @@ namespace Chernobyl2077Forms
         }
         public void WrAns()
         {
+            new System.Threading.ManualResetEvent(false).WaitOne(500);
             this.button5.Visible = false;
             this.button1.Visible = false;
             this.button2.Visible = false;
             this.button3.Visible = false;
             this.button4.Visible = false;
+            this.button6.Visible = true;
             this.label1.Visible = false;
             this.label3.Visible = false;
             this.label4.Visible = false;
             this.label5.Visible = false;
             this.label6.Visible = false;
             label7.Visible = true;
+            this.label13.Location = new System.Drawing.Point(384, 209);
+            this.label13.Size = new System.Drawing.Size(25, 13);
+            this.label12.Location = new System.Drawing.Point(327, 204);
+            this.label12.Size = new System.Drawing.Size(61, 20);
             while (BuIsPr == false)
             {
                 Application.DoEvents();
             }
+            BuIsPr = false;
+            score = 0;
+            label13.Text = "";
+            this.label13.Location = new System.Drawing.Point(704, 35);
+            this.label13.Size = new System.Drawing.Size(25, 13);
+            this.label12.Location = new System.Drawing.Point(646, 30);
+            this.label12.Size = new System.Drawing.Size(61, 20);
+            this.button5.Visible = false;
+            this.button1.Visible = true;
+            this.button2.Visible = true;
+            this.button3.Visible = true;
+            this.button4.Visible = true;
+            this.label1.Visible = true;
+            this.label3.Visible = true;
+            this.label4.Visible = true;
+            this.label5.Visible = true;
+            this.label6.Visible = true;
+            label7.Visible = false;
+            DeBuSetNoSlow();
+            Quiz1();
         }
         public void RiAns1()
         {
@@ -242,6 +315,7 @@ namespace Chernobyl2077Forms
                 BuIsPr = false;
                 BuIsPr1 = false;
                 this.button1.BackColor = System.Drawing.Color.LawnGreen;
+                score++;
             }
             if (BuIsPr2 == true)
             {
@@ -276,6 +350,7 @@ namespace Chernobyl2077Forms
                 BuIsPr = false;
                 BuIsPr2 = false;
                 this.button2.BackColor = System.Drawing.Color.LawnGreen;
+                score++;
             }
             if (BuIsPr1 == true)
             {
@@ -310,6 +385,7 @@ namespace Chernobyl2077Forms
                 BuIsPr = false;
                 BuIsPr3 = false;
                 this.button3.BackColor = System.Drawing.Color.LawnGreen;
+                score++;
             }
             if (BuIsPr1 == true)
             {
@@ -345,6 +421,7 @@ namespace Chernobyl2077Forms
                 BuIsPr = false;
                 BuIsPr4 = false;
                 this.button4.BackColor = System.Drawing.Color.LawnGreen;
+                score++;
             }
             if (BuIsPr1 == true)
             {
@@ -376,6 +453,12 @@ namespace Chernobyl2077Forms
         private void label7_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            BuIsPr = true;
+            this.button6.Visible = false;
         }
         //public void Timer1sec()
         //{
